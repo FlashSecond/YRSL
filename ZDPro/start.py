73d4f68db7ed499bfb12cc3e70d1a68ec8a19ea9
FlashSecond <FlashSecond@163.com>
2017-08-22 15:43:22 +0800

    upload

--- ZDPro/start.py
+++ ZDPro/start.py
@@ -9,9 +9,17 @@ def startload():
     #firpath ="cd /d %s &\
 #git pull origin master & exit" % (prepath)
     firpath = '\"' + prepath + '\\' + "upload.py\""
-    subprocess.check_call(firpath,shell = True)
-    secpath ='\"' + bepath + '\\' + "jiemian.pyw\""
-    os.system(secpath)
+    a=subprocess.Popen(firpath,shell = True)
+    while(a.poll() == None):
+        time.sleep(0.1)
+        print(a.poll())
+        pass
+    else:
+        print(a.poll())
+        secpath ='\"' + bepath + '\\' + "jiemian.pyw\""
+        subprocess.Popen(secpath,shell = True)
+        
+    
 if __name__ == '__main__':
     startload()
 
