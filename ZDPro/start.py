d65ffc059b70ce2702d8c0244ec26be39b2bab5b
FlashSecond <FlashSecond@163.com>
2017-08-23 11:22:31 +0800

    upload

--- ZDPro/start.py
+++ ZDPro/start.py
@@ -6,14 +6,18 @@ os.chdir(os.pardir)
 prepath = os.getcwd()
 os.chdir(bepath)
 def startload():
-    #firpath ="cd /d %s &\
+   # firpath ="cd /d %s &\
 #git pull origin master & exit" % (prepath)
     firpath = '\"' + prepath + '\\' + "upload.py\""
-    os.system(firpath)
+    a=subprocess.Popen(firpath,shell = True)
+    a.wait()
+
+def startup():
     secpath ='\"' + bepath + '\\' + "jiemian.pyw\""
     subprocess.Popen(secpath,shell = True)
         
     
 if __name__ == '__main__':
     startload()
+    startup()
 
