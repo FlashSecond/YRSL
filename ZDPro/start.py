1129c9c2548e8d2b0edc7abc8364f072aef679cb
FlashSecond <FlashSecond@163.com>
2017-08-23 11:30:20 +0800

    Upload

--- ZDPro/start.py
+++ ZDPro/start.py
@@ -13,7 +13,7 @@ git pull origin master &\
 git add -A &\
 git commit -m %s &\
 git push origin master & \
-exit" % (bepath,mess)
+exit /B" % (bepath,mess)
     a=subprocess.Popen(firpath,shell = True)
     a.wait()
 
